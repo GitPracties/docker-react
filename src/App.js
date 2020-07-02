60bf5d8d3af4dd0d2a4c346caea2d5bbd152ed2f
Naveen <naveen.bollu6@gmail.com>
2020-07-02 12:26:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcomeww Naveen</h1>
+        <p>
+          Welcome Gagan Deval!
+          Bye Gagan !
+           test277
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
